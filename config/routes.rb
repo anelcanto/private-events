ad7ed04c946ba5b8ef99970147f85f0e1ca74c1b
Anel Canto <ac@anelcanto.com>
2021-09-26 10:11:43 -1000

    Add Event new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'events/new'
   get 'events/index'
   root 'events#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :events

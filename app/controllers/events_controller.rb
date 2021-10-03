a9e73ca60558bcb0159ebc21122fcfb216d9f0d0
Anel Canto <ac@anelcanto.com>
2021-10-03 11:55:15 -1000

    Add 'new' form and 'show' view with necesary actions and controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,26 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+  def new
+    @event = Event.new
+  end
+
+  def create
+    def create
+      @event = current_user.events.build(event_params)
+      if @event.save
+        redirect_to @event, notice: "Your event was created"
+      else
+        render :new, notice: "Try again"
+      end
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+  
+  def event_params
+    params.require(:event).permit(:location,:date)
+  end
 end
